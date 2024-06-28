ac71fe936bde852abc07a4c23c88a69e279f922f
NickLP-RMT <nickneverdark@gmail.com>
2024-06-28 16:52:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,51 @@ function submitData() {
         console.error("เกิดข้อผิดพลาด:", error);
     });
 }
+function searchData() {
+    var formData = new FormData(document.getElementById("searchForm"));
+    var translatorName = formData.get("translatorName");
+    var date = formData.get("date");
 
+    fetch("https://script.google.com/macros/s/AKfycbzBxI35vuhxlTWqO6fsI-aKKMb_7SFG3E3THxjKxdGaon3fJg73ZV443aHt3Fe17blK/exec" + translatorName + "&date=" + date)
+        .then(response => response.json())
+        .then(data => {
+            var tbody = document.querySelector("#resultsTable tbody");
+            tbody.innerHTML = "";
+
+            data.records.forEach(record => {
+                var row = document.createElement("tr");
+
+                var bookingByCell = document.createElement("td");
+                bookingByCell.textContent = record.bookingBy;
+                row.appendChild(bookingByCell);
+
+                var translatorForWorkCell = document.createElement("td");
+                translatorForWorkCell.textContent = record.translatorForWork;
+                row.appendChild(translatorForWorkCell);
+
+                var translatorNameCell = document.createElement("td");
+                translatorNameCell.textContent = record.translatorName;
+                row.appendChild(translatorNameCell);
+
+                var dateCell = document.createElement("td");
+                dateCell.textContent = record.date;
+                row.appendChild(dateCell);
+
+                var timeFromCell = document.createElement("td");
+                timeFromCell.textContent = record.timeFrom;
+                row.appendChild(timeFromCell);
+
+                var timeToCell = document.createElement("td");
+                timeToCell.textContent = record.timeTo;
+                row.appendChild(timeToCell);
+
+                tbody.appendChild(row);
+            });
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+}
 function showPage(page) {
     var bookingPage = document.getElementById('booking');
     var searchingPage = document.getElementById('searching');
