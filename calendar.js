84ce80250a2c2b705b0ddb9320c05786748b2bfe
NickLP-RMT <nickneverdark@gmail.com>
2025-12-08 12:15:55 +0700

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -10,7 +10,7 @@ const span = document.getElementsByClassName("close")[0];
 const spinner = document.getElementById('spinner');
 const refreshButton = document.querySelector('.refresh');
 
-const API_URL = "https://script.google.com/macros/s/AKfycbzs7NdZGjjZTivIBIqR0OKRciTADISJGxQ9pGuriuwzmjaUaBng9HZfYSiqxFBQnUk/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzcCpIFWhFc2XNjGGDCYmpTzWQE2_IqUJL0YirH7LXIo_L96exXb5BVnoFGFgW96E8/exec";
 
 const INTERPRETER_MAP = {
   i001: "somSan",
